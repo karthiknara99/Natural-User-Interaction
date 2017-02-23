d97d10d0b80edbd383dcbdfada0f2043dc53ed5a
Karthik Narayanan <karthikn@ufl.edu>
2017-02-23 17:01:09 -0500

    Weekly View Coding Almost Done

--- ToDoListApp/app/src/main/java/com/teamnougat/todolistapp/WeeklyViewActivity.java
+++ ToDoListApp/app/src/main/java/com/teamnougat/todolistapp/WeeklyViewActivity.java
@@ -1,12 +1,22 @@
 package com.teamnougat.todolistapp;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.teamnougat.todolistapp.db.TaskContract;
 import com.teamnougat.todolistapp.db.TaskDbHelper;
 
 import java.text.SimpleDateFormat;
@@ -21,11 +31,14 @@ public class WeeklyViewActivity extends AppCompatActivity {
     private String sDate, eDate, sMonth;
     private Date newDate;
     private SimpleDateFormat dateFormat;
+    private float density;
     TextView[] myDay = new TextView[7];
     View[] myMarker = new View[7];
+    RelativeLayout[] myLayout = new RelativeLayout[7];
     TextView[] myMonth = new TextView[3];
     LinearLayout timeMarker;
     ViewGroup.MarginLayoutParams params;
+    ScrollView sv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +61,17 @@ public class WeeklyViewActivity extends AppCompatActivity {
         myMarker[mDay-1].setVisibility(View.VISIBLE);
 
         params = (ViewGroup.MarginLayoutParams) timeMarker.getLayoutParams();
-        float density = getResources().getDisplayMetrics().density;
-        int px = (int)(mMinute * density);
+        density = getResources().getDisplayMetrics().density;
+        final int px = (int)(mMinute * density);
         params.topMargin = px;
         timeMarker.setLayoutParams(params);
+
+        sv.post(new Runnable() {
+            @Override
+            public void run() {
+                sv.scrollTo(0, px-600);
+            }
+        });
 
         switch(mMonth+1){
             case 1: sMonth = "JAN";   break;
@@ -94,10 +114,30 @@ public class WeeklyViewActivity extends AppCompatActivity {
             eDate = dateFormat.format(newDate);
             myDay[i].setText( eDate.substring(8) );
         }
+        getTaskDetails();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.weekly_view_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override   //Done
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void getviews()
     {
+        sv = (ScrollView) findViewById(R.id.calendarScrollView);
         timeMarker = (LinearLayout) findViewById(R.id.currentTimeMarkerLinearLayout);
         myMonth[0] = (TextView)findViewById(R.id.currentMonthTextView1);
         myMonth[1] = (TextView)findViewById(R.id.currentMonthTextView2);
@@ -116,5 +156,59 @@ public class WeeklyViewActivity extends AppCompatActivity {
         myMarker[4] = (View)findViewById(R.id.thursdayMarkerView);
         myMarker[5] = (View)findViewById(R.id.fridayMarkerView);
         myMarker[6] = (View)findViewById(R.id.saturdayMarkerView);
+        myLayout[0] = (RelativeLayout) findViewById(R.id.sundayRelativeLayout);
+        myLayout[1] = (RelativeLayout) findViewById(R.id.mondayRelativeLayout);
+        myLayout[2] = (RelativeLayout) findViewById(R.id.tuesdayRelativeLayout);
+        myLayout[3] = (RelativeLayout) findViewById(R.id.wednesdayRelativeLayout);
+        myLayout[4] = (RelativeLayout) findViewById(R.id.thursdayRelativeLayout);
+        myLayout[5] = (RelativeLayout) findViewById(R.id.fridayRelativeLayout);
+        myLayout[6] = (RelativeLayout) findViewById(R.id.saturdayRelativeLayout);
+    }
+
+    private void getTaskDetails() {
+        SQLiteDatabase db = myHelper.getReadableDatabase();
+
+        String selectQuery = "SELECT " + TaskContract.TaskEntry.COL_TASK_TITLE + ", "
+                + TaskContract.TaskEntry.COL_TASK_TYPE + ", "
+                + TaskContract.TaskEntry.COL_TASK_DUEDAY + ", "
+                + TaskContract.TaskEntry.COL_TASK_DUETIME
+                + " FROM " + TaskContract.TaskEntry.TABLE
+                + " WHERE " + TaskContract.TaskEntry.COL_TASK_DUEDATE + " BETWEEN \"" + sDate +"\" AND \"" + eDate + "\";";
+
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                TextView msg = new TextView(WeeklyViewActivity.this);
+                msg.setBackgroundResource(R.color.medium_dark_gray);
+                msg.setText( cursor.getString(0) );
+                msg.setTextSize(15);
+                msg.setTextColor(getResources().getColor(R.color.strong_blue));
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (int)(60*density));
+                String[] input = cursor.getString(3).split(":");
+                int px = Integer.parseInt(input[0])*60 + Integer.parseInt(input[1]);
+                px = (int)( (px-60) * density );
+                params.setMargins(0, px, 0, 0);
+                msg.setLayoutParams(params);
+                msg.setGravity(Gravity.CENTER);
+
+                int layoutId = 0;
+                switch( cursor.getString(2) )
+                {
+                    case "Sun": layoutId = 0;   break;
+                    case "Mon": layoutId = 1;   break;
+                    case "Tue": layoutId = 2;   break;
+                    case "Wed": layoutId = 3;   break;
+                    case "Thu": layoutId = 4;   break;
+                    case "Fri": layoutId = 5;   break;
+                    case "Sat": layoutId = 6;   break;
+                }
+
+                myLayout[layoutId].addView(msg);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
     }
 }
